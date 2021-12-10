3ce566d3a3c0f2135e6dd9ceb5bd6a3651828012
Aaronn Kelly <aaronn.k@gmail.com>
2021-12-10 22:52:00 +0000

    partly working - bad maths?

--- 2021/10/10.py
+++ 2021/10/10.py
@@ -6,28 +6,71 @@ import timeit
 from typing import ByteString
 
 
+chars = {"(":")","[":"]","{":"}","<":">"}
+scores = {")":3,"]":57,"}":1197,">":25137}
+
 def getthing():
     thing = []
-    with open("./10/example.txt", "r") as f:
+    with open("./2021/10/example.txt", "r") as f:
         for line in f.readlines():
             thing.append((line.strip()))
     return thing
 
 
-def parsething(thing):
-    return thing
+def filterIncomplete(thing):
+    output=[]
+    for line in thing:
+        if isLineComplete(line):
+            output.append(line)
+    return output
 
-def parsething2(thing):
-    for x in range(len(thing)):
-        thing[x] = [i for i in thing[x]]
-    return thing
+
+def isLineComplete(line):
+    if len(line) % 2 ==0:
+        return True
+    else:
+        return False
+
+def isOpening(string):
+    if string in ['(','[','{','<']:
+        return True
+def isClosing(string):
+    if string in [')',']','}','>']:
+        return True
+
+def findClosing(line,open=''):
+    # {()()()>
+    chars = {"(":")","[":"]","{":"}","<":">"}
+
+    if open=='':
+        if isOpening(line[0]):
+            x = findClosing(line[1:],line[0])
+            return x
+        elif isClosing(line[0]):
+            return scores[line[0]]
+    elif isOpening(line[0]):
+        x =  findClosing(line[1:],line[0])
+        return x
+    elif isClosing(line[0]):
+        if chars[open] == line[0]:
+            x= isClosing(line[1:])
+        else:
+            return scores[line[0]]
+    
+
+
+
 
 def solve1(thing):
-    total = 0
+    total =0
+    for l in thing:
+        total +=findClosing(l)
+    return total
+    
 
 
 def PartOne():
-    thing = parsething(getthing())
+    thing = filterIncomplete(getthing())
     res = solve1(thing)
 
     print("PartOne")
@@ -37,9 +80,6 @@ def PartOne():
 def PartTwo():
 
     print("PartTwo")
-    thing = parsething2(getthing())
-    res = solv2e2(thing)
-    print(res)
 
 
 PartOne()
